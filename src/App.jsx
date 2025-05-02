bf7923f6b3eb2c91b58d91e24485487513121025
EjikeOdoh <ejike.developer@gmail.com>
2025-05-02 12:32:14 +0100

    Complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import QRImg from './assets/image-qr-code.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>
-      Hello world
-    </h1>
+    <div className="card">
+      <div className="img-container">
+        <img src={QRImg} />
+      </div>
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
   )
 }
 
